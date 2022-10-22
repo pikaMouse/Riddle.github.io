822b41c39d508e75f3c6a029591e40cf8cbc2bec
pikaMouse <58113937@qq.com>
2022-10-23 02:49:35 +0800

    change: 完成阶段1

---
+++ member.js
@@ -0,0 +1,87 @@
+var member = [
+	{
+		dateTime: '2008年12月30日',
+		desc: '时任浙江省委常委、市委书记王国平，市长蔡奇，市委常委、区委书记朱金坤，区长姜军为杭州市余杭区余杭组团（创新基地）党工委、管委会揭牌。',
+		imgSrc: 'img/big/1_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2010年5月10日',
+		desc: '时任省委常委、常务副省长陈敏尔调研余杭组团，实地踏看余杭组团科创中心、中央公园、海创园、阿里巴巴淘宝城、五常北部园区等项目现场。',
+		imgSrc: 'img/big/2_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2010年7月13日',
+		desc: '时任省领导黄坤明、市长蔡奇、陈加元出席参加浙江海外高层次人才创新园的挂牌仪式。',
+		imgSrc: 'img/big/3_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2011年2月14日',
+		desc: '时任省委书记赵洪祝，省长吕祖善莅临余杭组团（创新基地）指导工作。',
+		imgSrc: 'img/big/4_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2012年5月24日',
+		desc: '时任省委常委、组织部长蔡奇一行调研杭州未来科技城。',
+		imgSrc: 'img/big/5_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2012年7月11日',
+		desc: '时任省委副书记、省长夏宝龙调研未来科技城，实地考察了尚越光电及海创园首期工程。',
+		imgSrc: 'img/big/6_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2012年8月1日',
+		desc: '时任省委书记、省人大常委会主任赵洪祝专题调研指导未来科技城高科技人才创业企业发展情况等科技创新工作。',
+		imgSrc: 'img/big/7_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2013年6月5日',
+		desc: '时任省委副书记、省长李强就贯彻落实省委十三届三次全会精神、全面实施创新驱动发展战略赴杭州未来科技城进行工作指导。',
+		imgSrc: 'img/big/8_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2013年11月5日',
+		desc: '时任省委常委、市委书记龚正赴杭州未来科技城考察。',
+		imgSrc: 'img/big/9_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2014年2月19日',
+		desc: '国家工商总局副局长马正其、总局消保局局长杨红灿、中消协秘书长常宁一行莅临杭州未来科技城指导工作。',
+		imgSrc: 'img/big/10_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2015年4月1日',
+		desc: '时任副省长郑继伟一行来到梦想小镇先导区进行实地考察调研。',
+		imgSrc: 'img/big/11_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2015年6月2日',
+		desc: '时任省委书记夏宝龙一行赴未来科技城考察调研梦想小镇建设情况。',
+		imgSrc: 'img/big/12_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2016年4月23日',
+		desc: '时任全国政协副主席、科技部部长万钢调研梦想小镇建设。',
+		imgSrc: 'img/big/13_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2016年5月30日',
+		desc: '时任省长李强赴未来科技城调研金融小镇（规划）、梦想小镇及杭州虚现科技、杭州非白三维科技等。',
+		imgSrc: 'img/big/14_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2017年3月10日',
+		desc: '时任省委常委、组织部长任振鹤莅临未来科技城指导人才、党建工作。',
+		imgSrc: 'img/big/15_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2017年7月9日',
+		desc: '省委副书记、省长袁家军参加中国人工智能小镇启动仪式。',
+		imgSrc: 'img/big/16_window_pic_xxhdpi.jpg'
+	},
+	{
+		dateTime: '2017年9月6日',
+		desc: '时任全国人大常委会原副委员长路甬祥、省委书记车俊为之江实验室揭牌并讲话，省长袁家军为之江实验室负责人颁发聘书。',
+		imgSrc: 'img/big/17_window_pic_xxhdpi.jpg'
+	}
+];
